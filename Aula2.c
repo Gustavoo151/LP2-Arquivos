2e6540c9cc845839638e7bd722e89d4a646384ad
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2022-07-21 17:01:37 -0300

    Update Aula2.c

--- Aula2.c
+++ Aula2.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+int cpf(){
+    int cpf =0;
+    printf("Digite seu CPF: \n[-1 lista todos os assentos vazios] \n[0 finaliza o programa]: ");
+    scanf("%d", &cpf);
+
+    return cpf;
+}
+
+int main() {
+    int i, c;
+    int assentos[24];
+
+
+    for(i=0; i< 24; i++){
+        assentos[i] = 0;
+    }
+
+    if(cpf == -1){
+        for(i=0; i<24; i++){
+            if (assentos[i] == 0){
+                printf("Assento na posicao %d vazio\n", i);
+            }
+        }
+    }
+
+    else if(cpf == 0){
+        printf("parar");
+    }
+    else{
+        for(c=0; c <2; c++){
+            for(i=0; i<24; i++){
+                    if(i% 2 == 1){
+                        if (assentos[i] == 0){
+                            assentos[i] = cpf();
+                            printf("Assento reservado ao lado da janela na posicao %i\n", i);
+                }
+            }
+        }
+    }
+}
+    return 0;
+}
+
+// Falta colocar em ordem os if's, chamar a função cpf nas condicionais, consertar o if e mostrar o mapa, cancelar passegens e  
