ffdd1445d0956d2aecf9007f3728e3def39f5530
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2022-08-25 01:49:55 -0300

    24/08

--- Aula 03 - /Aula2.c
+++ Aula 03 - /Aula2.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
 
-int cpf(){
-    int cpf =0;
-    printf("Digite seu CPF: \n[-1 lista todos os assentos vazios] \n[0 finaliza o programa]: ");
-    scanf("%d", &cpf);
+int main(){
 
-    return cpf;
-}
-
-int main() {
-    int i, c;
-    int assentos[24];
+    int assentos[24] = {0}, cpf = 0, contAssento = 0, opcMenu = 1;
 
 
-    for(i=0; i< 24; i++){
-        assentos[i] = 0;
-    }
+    do{
+        printf("\nMENU: \n1- Adicionar assento: \n2- Listar assentos Vazios: \n3- Cancelar passagem: \n0- Finalizar: \n");
+        scanf("%d", &opcMenu);
 
-    if(cpf == -1){
-        for(i=0; i<24; i++){
-            if (assentos[i] == 0){
-                printf("Assento na posicao %d vazio\n", i);
-            }
-        }
-    }
+        switch (opcMenu){
 
-    else if(cpf == 0){
-        printf("parar");
-    }
-    else{
-        for(c=0; c <2; c++){
-            for(i=0; i<24; i++){
-                    if(i% 2 == 1){
+            case 1:
+                printf("Digite seu CPF: ");
+                scanf("%d", &cpf);
+
+                if (contAssento < 13){
+                    for (int i = 1; i < 24; i += 2){
                         if (assentos[i] == 0){
-                            assentos[i] = cpf();
-                            printf("Assento reservado ao lado da janela na posicao %i\n", i);
+                            assentos[i] = cpf;
+                            printf("<%d> -- Assento #<%d> (Janela)\n", cpf, i);
+                            contAssento ++;
+                            printf("%d \n", contAssento);
+                            break;
+                        }  
+                    }
                 }
+
+                if (contAssento > 12){
+                    for(int j = 0; j < 24; j += 2){
+                        if (assentos[j] == 0){
+                            assentos[j] = cpf;
+                            printf("<%d> -- Assento #<%d> (Corredor)\n", cpf, j);
+                            break;
+                            }
+                        }
+                    }
+                    break;
+
+            case 2:
+                for (int i = 0; i < 24; i ++){
+                    if (assentos[i] == 0)
+                        printf("Assento %d vazio.\n", i);
+                }
+                break;
+
+            case 0:
+                break;
+            
+            default:
+                printf("Opção inválida.\n\n");
             }
-        }
-    }
-}
+
+        }while(opcMenu != 0);
+    
     return 0;
 }
 
-/* Falta colocar em ordem os if's, chamar a função cpf nas condicionais,
-consertar o if e mostrar o mapa, cancelar passegens e  
-fazer o controle de assentos com tamanho do vertor para não dar estouro de memória */
+// Consertar if pq está pegando o mesmo valor no cont 13
